e2ff5afc9bc341c4fa2ca8810be70dd7d82d19d9
GopiCharanReddy <gopicharanvgcr@gmail.com>
2025-12-11 00:39:25 +0530

    Add Message table and update Prisma schema; refactor database client setup

--- packages/db/src/client.js
+++ packages/db/src/client.js
@@ -1,9 +1,8 @@
 import "dotenv/config";
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from "../generated/prisma/index.js";
-declare const prisma: PrismaClient<{
-    adapter: PrismaPg;
-}, never, import("../generated/prisma/runtime/client.js").DefaultArgs>;
+const connectionString = `${process.env.DATABASE_URL}`;
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
 export { prisma };
 export default prisma;
-//# sourceMappingURL=client.d.ts.map
