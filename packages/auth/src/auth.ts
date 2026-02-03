484b8898bba1451c14b5d2f348ecc0e31f088114
GopiCharanReddy <gopicharanvgcr@gmail.com>
2026-02-04 00:28:08 +0530

    add better-auth-secret

--- packages/auth/src/auth.ts
+++ packages/auth/src/auth.ts
@@ -3,6 +3,8 @@ import { prismaAdapter } from 'better-auth/adapters/prisma'
 import prismaClient from '@repo/db'
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL,
+  secret: process.env.BETTER_AUTH_SECRET,
   database: prismaAdapter(prismaClient, {
     provider: "postgresql"
   }),
