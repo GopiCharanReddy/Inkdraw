376bea396773ec197a3c88a72a3e1e8d9c9e3932
GopiCharanReddy <gopicharanvgcr@gmail.com>
2026-02-03 18:52:48 +0530

    Add ca-certificates

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -6,7 +6,15 @@ import { PrismaClient } from "../generated/prisma/index.js";
 const connectionString = `${process.env.DATABASE_URL}`
 
 const prismaClientSingleton = () => {
-  const pool = new Pool({ connectionString });
+  const pool = new Pool({
+    connectionString,
+    max: 20,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
   const adapter = new PrismaPg(pool);
 
   return new PrismaClient({ adapter });
