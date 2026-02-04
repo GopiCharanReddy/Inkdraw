7717ad5634c4fc55229c81f274d60ea2da974659
GopiCharanReddy <gopicharanvgcr@gmail.com>
2026-02-04 20:29:48 +0530

    implement lazy initialization for better-auth-secret

--- apps/web/app/api/auth/[...all]/route.ts
+++ apps/web/app/api/auth/[...all]/route.ts
@@ -1,4 +1,5 @@
-import { auth } from "@repo/auth/server";
+import { getAuth } from "@repo/auth/server";
 import { toNextJsHandler } from "better-auth/next-js"
 
+const auth = getAuth();
 export const { GET, POST } = toNextJsHandler(auth);
--- packages/auth/src/auth.ts
+++ packages/auth/src/auth.ts
@@ -2,19 +2,29 @@ import { betterAuth } from 'better-auth'
 import { prismaAdapter } from 'better-auth/adapters/prisma'
 import prismaClient from '@repo/db'
 
-export const auth = betterAuth({
-  baseURL: process.env.BETTER_AUTH_URL,
-  secret: process.env.BETTER_AUTH_SECRET,
-  database: prismaAdapter(prismaClient, {
-    provider: "postgresql"
-  }),
-  emailAndPassword: {
-    enabled: true,
-  },
-  socialProviders: {
-    google: {
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    },
-  },
-})
+let _auth: ReturnType<typeof betterAuth> | null = null;
+
+export const getAuth = () => {
+  if (!_auth) {
+    if (!process.env.BETTER_AUTH_SECRET || !process.env.BETTER_AUTH_URL) {
+      throw new Error("BETTER_AUTH_SECRET or BETTER_AUTH_URL is missing at runtime");
+    }
+    _auth = betterAuth({
+      baseURL: process.env.BETTER_AUTH_URL,
+      secret: process.env.BETTER_AUTH_SECRET,
+      database: prismaAdapter(prismaClient, {
+        provider: "postgresql"
+      }),
+      emailAndPassword: {
+        enabled: true,
+      },
+      socialProviders: {
+        google: {
+          clientId: process.env.GOOGLE_CLIENT_ID as string,
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        },
+      },
+    })
+  }
+  return _auth;
+}
