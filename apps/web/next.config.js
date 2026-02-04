a297e71045e67c628761bdbbbc245713a6a71f68
GopiCharanReddy <gopicharanvgcr@gmail.com>
2026-02-04 18:59:21 +0530

    fix: remove env key from next.config.js, update authclient & add database_url as arg and run in dockerfile

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,11 +2,6 @@
 const nextConfig = {
   transpilePackages: ["@repo/ui", "@repo/auth"],
   output: "standalone",
-  env: {
-    BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
-    BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
-    DATABASE_URL: process.env.DATABASE_URL,
-  },
 };
 
 export default nextConfig;
